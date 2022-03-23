d05269adf4da86b2afd01a35d89742a1cc66d765
James Morical <77235874+jkmor31@users.noreply.github.com>
2022-03-23 13:26:42 -0500

    enable cors g

--- src/main/java/com/falcon/stocks/StocksApplication.java
+++ src/main/java/com/falcon/stocks/StocksApplication.java
@@ -2,6 +2,10 @@ package com.falcon.stocks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class StocksApplication {
@@ -9,5 +13,13 @@ public class StocksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StocksApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/products").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
 }
